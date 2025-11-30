c2b3ceb6e0777b046992ec02bbeca2048fbf3944
Kyle O. <imjustsopro@gmail.com>
2025-11-29 23:54:06 -0500

    Move operating system services into separate definition.

--- system/install.scm
+++ system/install.scm
@@ -39,22 +39,22 @@
 
 ;; https://substitutes.nonguix.org/signing-key.pub
 (define %signing-key
-  (plain-file "nonguix.pub" "\
-(public-key
+  (plain-file "nonguix.pub"
+   "(public-key
  (ecc
   (curve Ed25519)
   (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))"))
 
 (define %channels
   (cons* (channel
-          (name 'nonguix)
-          (url "https://gitlab.com/nonguix/nonguix")
-          ;; Enable signature verification:
-          (introduction
-           (make-channel-introduction
-            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-            (openpgp-fingerprint
-             "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+           (name 'nonguix)
+           (url "https://gitlab.com/nonguix/nonguix")
+           ;; Enable signature verification:
+           (introduction
+            (make-channel-introduction
+             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+             (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
          %default-channels))
 
 (define installation-os-nonfree
@@ -69,23 +69,20 @@
     (kernel-arguments '("quiet" "modprobe.blacklist=radeon" "net.ifnames=0"))
 
     (services
-     (cons*
-      (modify-services (operating-system-user-services installation-os)
-        (guix-service-type
-         config => (guix-configuration
-                    (inherit config)
-                    (guix (guix-for-channels %channels))
-                    (authorized-keys
-                     (cons* %signing-key
-                            %default-authorized-guix-keys))
-                    (substitute-urls
-                     `(,@%default-substitute-urls
-                       "https://substitutes.nonguix.org"))
-                    (channels %channels))))))
+     (cons* (modify-services (operating-system-user-services installation-os)
+              (guix-service-type config =>
+                                 (guix-configuration (inherit config)
+                                                     (guix (guix-for-channels
+                                                            %channels))
+                                                     (authorized-keys (cons*
+                                                                       %signing-key
+                                                                       %default-authorized-guix-keys))
+                                                     (substitute-urls `(,@%default-substitute-urls
+                                                                        "https://substitutes.nonguix.org"))
+                                                     (channels %channels))))))
 
     ;; Add some extra packages useful for the installation process
-    (packages
-     (append (list git curl neovim emacs-no-x-toolkit)
-             (operating-system-packages installation-os)))))
+    (packages (append (list git curl neovim emacs-no-x-toolkit)
+                      (operating-system-packages installation-os)))))
 
 installation-os-nonfree
